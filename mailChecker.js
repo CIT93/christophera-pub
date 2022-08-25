4e722cad55ea736259cbf1d540ce42e0b913625f
Christopher Acosta <codkagamine07@gmail.com>
2022-08-25 03:16:13 -0700

    Concept for w3 Code

---
+++ mailChecker.js
@@ -0,0 +1,15 @@
+const showOnPage = function (text) {
+    let newParagraph = document.createElement("p")
+    newParagraph.innerHTML = text
+    let outputDiv = document.getElementById("output")
+    outputDiv.append(newParagraph)
+  }
+
+  //Mail Checking code
+  //print "Should I check the mail today?"
+  //function one: Is it the right day to check mail
+  //print message indicating whether to check mail or not
+  //function two: Am I expecting something?
+  //print message indicating whether to be on the lookout for packages or not.
+
+  showOnPage('Hello World')
